318d1ee70444ec0acb9d2ee2b458937b72865281
Joao Gomes <joaopfg.2002@gmail.com>
2022-03-21 23:34:05 +0000

    Responsive Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -79,7 +79,7 @@ export default function Dashboard() {
           alignContent='flex-start'
         >
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
